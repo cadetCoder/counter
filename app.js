e686bba1ac8126eedd32ca8e90b917d777475899
Darrel Toledo <darreltoledo@icloud.com>
2021-10-14 22:00:18 +0100

    when number reset the value will change to black

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ btns.forEach(function (btn) {
     if (count < 0) {
       value.style.color = "red"
     }
+    if (count === 0) {
+      value.style.color = "#222"
+    }
     value.textContent = count
   })
 })
