28130939cedc3804db2cdc32937694dea6892a5b
Darrel Toledo <darreltoledo@icloud.com>
2021-10-14 21:50:22 +0100

    if state for decrease btn when click, value reduce

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
-  btn.adEventListener('click', function(e){
-    
+  btn.addEventListener('click', function(e){
+    const styles = e.currentTarget.classList
+    if (styles.contains("decrease")) {
+      count--
+    }
+    value.textContent = count
   })
 })
